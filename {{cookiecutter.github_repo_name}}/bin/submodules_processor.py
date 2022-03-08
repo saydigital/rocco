1f57ecf1bc38a90002352cbc4be3597942478c15
Dario Del Zozzo <dario.delzozzo@gmail.com>
2022-03-08 18:15:12 +0100

    [FIX] compose https clone link from ssh instead of submodule name

--- {{cookiecutter.github_repo_name}}/bin/submodules_processor.py
+++ {{cookiecutter.github_repo_name}}/bin/submodules_processor.py
@@ -16,8 +16,13 @@ if __name__ == '__main__':
     repo = Repo()
     for submodule in repo.iter_submodules():
         if submodule.url.startswith("git@"):
+            # All github SSH repos are like git@github.com:<repo_name>.git
+            github_string = "@github.com:"
+            base_index = submodule.url.find(github_string)
+            repo_infos_index = base_index + len(github_string)
+            repo_infos_str = submodule.url[repo_infos_index:]
             key = f'submodule "{submodule.name}"'
-            value = f'https://{GITHUB_TOKEN}@github.com/{submodule.name}.git'
+            value = f'https://{GITHUB_TOKEN}@github.com/{repo_infos_str}'
             repo.config_writer().set_value(key, 'url', value).release()
     # This must be necessarily done via subprocess because GitPython will use
     # the URL found in .gitmodules
