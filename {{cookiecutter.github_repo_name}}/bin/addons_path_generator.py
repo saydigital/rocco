e8f243b56dec7e2c8e4e62d992da7c3145e5f051
Dario Del Zozzo <dario.delzozzo@gmail.com>
2022-03-08 21:06:30 +0100

    [IMP] Add possibility to add custom addons_path in a yaml file

--- {{cookiecutter.github_repo_name}}/bin/addons_path_generator.py
+++ {{cookiecutter.github_repo_name}}/bin/addons_path_generator.py
@@ -7,6 +7,7 @@ import sys
 
 from configparser import ConfigParser
 from git import Repo
+import yaml
 
 ADDONS_PATH = sys.argv[1]
 ENTERPRISE_PATH = sys.argv[2]
@@ -29,6 +30,13 @@ if __name__ == '__main__':
     if submodules:
         submodules = ','.join(sorted(submodules))
         paths.append(submodules)
+    # Check YAML FILE if custom PATHS are inserted
+    with open('/rocco/etc/extra_addons_path.yaml', 'r') as extra_addons_path:
+        extra_addons_file = yaml.load(extra_addons_path, Loader=yaml.FullLoader)
+        extra_addons_list = extra_addons_file.get("addons_path")
+        if extra_addons_list:
+            paths.extend(extra_addons_list)
+    # Generate the comma-separated addons path
     addons_path = ','.join(paths)
 
     parser = ConfigParser()
